10f99aa43d1155405550ed3995f3ff87842038a9
newyorkthink <87791643+newyorkthink@users.noreply.github.com>
2026-08-23 07:00:54 +0800

    Fix KeePass Keebuntu release asset lookup

--- keepass/GtkStatusIcon.sh
+++ keepass/GtkStatusIcon.sh
@@ -2,13 +2,25 @@
 set -e
 
 # 下载 newyorkthink/Keebuntu 最新正式 Release 中的 GTK2 XEmbed 托盘插件，并校验 SHA256SUMS。
+# Keebuntu 使用固定 latest Release 标签，实际压缩包文件名仍包含 VERSION，因此不能直接用 tagName 推导文件名。
 KEEBUNTU_REPOSITORY="newyorkthink/Keebuntu"
 KEEBUNTU_TAG="$(gh release view --repo "$KEEBUNTU_REPOSITORY" --json tagName --jq '.tagName')"
 test -n "$KEEBUNTU_TAG"
 
-KEEBUNTU_VERSION="${KEEBUNTU_TAG#v}"
+KEEBUNTU_ARCHIVE="$(
+  gh release view "$KEEBUNTU_TAG" \
+    --repo "$KEEBUNTU_REPOSITORY" \
+    --json assets \
+    --jq '.assets[].name | select(startswith("keebuntu-gtk-status-icon-") and endswith(".tar.gz"))' \
+    | head -n 1
+)"
+test -n "$KEEBUNTU_ARCHIVE"
+
+KEEBUNTU_VERSION="${KEEBUNTU_ARCHIVE#keebuntu-gtk-status-icon-}"
+KEEBUNTU_VERSION="${KEEBUNTU_VERSION%.tar.gz}"
+test -n "$KEEBUNTU_VERSION"
+
 KEEBUNTU_WORK_DIR="/tmp/keebuntu-gtk-status-icon"
-KEEBUNTU_ARCHIVE="keebuntu-gtk-status-icon-${KEEBUNTU_VERSION}.tar.gz"
 KEEBUNTU_PAYLOAD_DIR="$KEEBUNTU_WORK_DIR/keebuntu-gtk-status-icon-${KEEBUNTU_VERSION}"
 
 rm -rf "$KEEBUNTU_WORK_DIR"
